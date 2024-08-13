0a1ca7abd4322fe0727cae6c4146d353f9111a58
Vardoxx <maksgavrilov956@gmail.com>
2024-08-13 22:13:54 +0300

    small refining

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -2,17 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import HomePage from "../pages/Home-page/HomePage";
 import LibraryPage from "../pages/Library-page/LibraryPage";
+import FavoritePage from "../pages/Favorite-page/FavoritePage";
 
 const Router = () => {
   return (
     <div className="wrapper">
       <Routes>
         <Route
-          //prettier-ignore
           element={<Layout />}
+          //prettier-ignore
           children={[
             <Route key="home" index element={<HomePage />} />,
             <Route key="library" path="/library" element={<LibraryPage />} />,
+            <Route key="favorites" path="/favorites" element={<FavoritePage />}/>,
           ]}
         />
       </Routes>
---
+++ src/pages/Favorite-page/FavoritePage.tsx
@@ -0,0 +1,11 @@
+import s from "./FavoritePage.module.scss";
+
+const FavoritePage = () => {
+  return (
+    <div className="_container pt36">
+      <div className={s.library_page}></div>
+    </div>
+  );
+};
+
+export default FavoritePage;
